ba1733f00a3b4d517be5560f045e691337488388
johngrotts <johnsmaillist@gmail.com>
2022-02-11 21:40:02 -0600

    SP-BACK First commit Adding spring starter Adding gitignore

---
+++ src/main/java/com/steampipe/steampipe/SteampipeApplication.java
@@ -0,0 +1,13 @@
+package com.steampipe.steampipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteampipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteampipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steampipe/steampipe/SteampipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steampipe.steampipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteampipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
